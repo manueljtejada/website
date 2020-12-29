e50120ff6b59880c45e683b133da7f5b27b3fbe7
Manuel Tejada <manute32@gmail.com>
2020-12-29 21:12:29 +0100

    Adds initial theme and global styles

---
+++ gatsby-browser.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import theme, { GlobalStyle } from "./src/components/ui/theme";
+
+export const wrapRootElement = ({ element }) => (
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    {element}
+  </ThemeProvider>
+)
---
+++ src/components/ui/theme.tsx
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from "styled-components"
+import reset from 'styled-reset'
+
+export default {
+  colors: {
+    primary: {
+      main: "#52BBDE"
+    },
+    secondary: {
+      main: "#ED726D",
+      dark: "#E66E6A"
+    },
+    white: "#fff",
+    black: "#000",
+    gray: {
+      light: "#fafafa",
+    },
+    text: {
+      primary: "#000",
+      secondary: "#666666"
+    }
+  }
+}
+
+export const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  *, *:before, *:after {
+    box-sizing: border-box;
+  }
+  html {
+    box-sizing: border-box;
+    scroll-behavior: smooth;
+    font-family: -apple-system, Roboto, sans-serif, serif;
+    font-size: 16px;
+    color: #1a202c;
+  }
+  body {
+    line-height: 1.5;
+    letter-spacing: 0;
+  }
+`
