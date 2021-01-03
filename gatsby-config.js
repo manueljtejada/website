17e8b6fe08643fb64edf9b1bf16c0e6a7f1f55f5
Manuel Tejada <manute32@gmail.com>
2021-01-03 14:11:45 +0100

    Adds prismic source and schema

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,18 @@ module.exports = {
         trackingId: "UA-98482386-1",
       },
     },
+    {
+      resolve: "gatsby-source-prismic",
+      options: {
+        repositoryName: "manueljtejada",
+        accessToken:
+          "MC5YLTJyYWhBQUFDUUFrdFg0.ZO-_ve-_vSbvv70QciUl77-9QR5XL--_vTXvv71NFO-_vWXvv73vv73vv70YU--_ve-_ve-_ve-_ve-_ve-_vQ",
+        schemas: {
+          blog_post: require("./src/schemas/blog_post.json"),
+          project: require("./src/schemas/project.json"),
+        },
+      },
+    },
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
